022244d126e324b7a63c337c6adbe8b9753cc61b
3nigma-tik <cole.blackwood@icloud.com>
2021-11-04 09:45:43 -0700

    added first component

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = [
+
+        ];
+    this.state = {
+        newPerson: ''
+    };
+    }
+
+    render(){
+        return(
+            <h1>Component Exercises from Component</h1>
+        )
+    }
+
+}
+    
+export default App;
---
+++ src/components/DisplayName.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class DisplayName extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: 'Reggie',
+            lastName: 'White'
+        }
+    }
+}
+
+export default DisplayName;
